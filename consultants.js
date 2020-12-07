1d1eb80e9dcae42b11ca8eae3fc8842650c6ba5d
Stef Van Dingenen <stefvandingenen@hotmail.com>
2020-12-07 15:37:09 +0100

    add dependencies

---
+++ consultants.js
@@ -0,0 +1,52 @@
+//consultants.js
+module.exports = [
+  {
+    name: "Jos Verstappen",
+    price: 999.99,
+    image:
+      "https://www.dotnetlab.eu/wp-content/uploads/2020/10/0-4.jpg",
+    description: "The man, the myth, the legend",
+  },
+  {
+    name: "Ive Verstappen",
+    price: 799.99,
+    image:
+      "https://www.dotnetlab.eu/wp-content/uploads/2020/10/0.jpg",
+    description: "Not related to Max Verstappen, but can pingpong",
+  },
+  {
+    name: "Bjorn Ruysen",
+    price: 649.99,
+    image:
+      "https://www.dotnetlab.eu/wp-content/uploads/2020/11/Je-1000x1000-1-600x450.jpg",
+    description: "The Bear Grylls from Heultje",
+  },
+  {
+    name: "Daan Geudens",
+    price: 549.99,
+    image:
+      "https://www.dotnetlab.eu/wp-content/uploads/2020/12/daan_linkedin.jpg",
+    description: "Works hard when he's not on his bike. Or running. Or swimming.",
+  },
+  {
+    name: "Tom Peeters",
+    price: 499.99,
+    image:
+      "https://www.dotnetlab.eu/wp-content/uploads/2020/10/0-2.jpg",
+    description: "I'm sure I have already met this person somewhere",
+  },
+  {
+    name: "Stef Van Dingenen",
+    price: 9.99,
+    image:
+      "https://scontent-bru2-1.cdninstagram.com/v/t51.2885-19/s150x150/129354814_385540312872526_7980020720549186532_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_ohc=Rq6fSpBpdjgAX80G9st&tp=1&oh=f0bc60fb501c77d4e6ad131d26d89e92&oe=5FF668D0",
+    description: "Doing his best. Good personality",
+  },
+  {
+    name: "Jarne Peeters",
+    price: -9.99,
+    image:
+      "https://scontent-bru2-1.xx.fbcdn.net/v/t1.0-9/31543766_10214950647255219_5597626783772966912_n.jpg?_nc_cat=104&ccb=2&_nc_sid=174925&_nc_ohc=TL5GF9v-lBYAX-22bNl&_nc_ht=scontent-bru2-1.xx&oh=89a483507bc3f141e3d0a4a64417b185&oe=5FF3CA5B",
+    description: "Creates bugs so that the new people can learn the codebase",
+  },
+];
