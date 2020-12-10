712084e9db7ba9e90690ac3c9e3554f29b222c53
Stef Van Dingenen <stefvandingenen@hotmail.com>
2020-12-10 19:09:04 +0100

    remove unused code

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,13 +1,5 @@
-import { ConsultantsState, initialConsultantsState } from './consultants/state';
+import { ConsultantsState } from './consultants/state';
 
 export interface AppState {
     consultantsState: ConsultantsState;
-}
-
-export const initialAppState: AppState = {
-    consultantsState: initialConsultantsState,
-}
-
-export function getInitialState(): AppState {
-    return initialAppState;
 }
