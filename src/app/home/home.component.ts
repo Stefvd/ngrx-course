6e76e05c8f75d5b584c4fcb12af511bcbad4988b
Stef Van Dingenen <stefvandingenen@hotmail.com>
2020-12-16 10:50:27 +0100

    return observable in consultantService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { ConsultantService } from '../services/consultant.service';
+import { AppState } from '../store/app.state';
+import { UpdateConsultants } from '../store/consultants/actions';
+import { Consultant } from '../store/models';
 
 @Component({
   selector: 'app-home',
@@ -32,9 +36,12 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private consultantService: ConsultantService,
+    private store: Store<AppState>,
   ) {}
 
   public ngOnInit(): void {
-    this.consultantService.updateConsultants();
+    this.consultantService.getConsultants().subscribe((consultants: Consultant[]) => {
+      this.store.dispatch(UpdateConsultants(consultants));
+    });
   }
 }
--- src/app/services/consultant.service.ts
+++ src/app/services/consultant.service.ts
@@ -1,9 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../store/app.state';
-import { UpdateConsultants } from '../store/consultants/actions';
-import { Consultant } from '../store/models';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +8,9 @@ export class ConsultantService {
 
   constructor(
     private httpClient: HttpClient,
-    private store: Store<AppState>,
   ) { }
 
-  public updateConsultants(): void {
-    this.httpClient.get('http://localhost:4000/consultants').subscribe((consultants: Consultant[]) => {
-      this.store.dispatch(UpdateConsultants(consultants));
-    });
+  public getConsultants() {
+    return this.httpClient.get('http://localhost:4000/consultants');
   }
 }
