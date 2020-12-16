9146d78f5ec5d3b9384d19fe832be31984c0758c
Stef Van Dingenen <stefvandingenen@hotmail.com>
2020-12-16 11:11:08 +0100

    replace subscribe on getConsultants

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { ConsultantService } from '../services/consultant.service';
-import { AppState } from '../store/app.state';
-import { UpdateConsultants } from '../store/consultants/actions';
-import { Consultant } from '../store/models';
 
 @Component({
   selector: 'app-home',
@@ -34,14 +29,7 @@ export class HomeComponent implements OnInit {
     },
   ];
 
-  constructor(
-    private consultantService: ConsultantService,
-    private store: Store<AppState>,
-  ) {}
+  constructor() {}
 
-  public ngOnInit(): void {
-    this.consultantService.getConsultants().subscribe((consultants: Consultant[]) => {
-      this.store.dispatch(UpdateConsultants(consultants));
-    });
-  }
+  public ngOnInit(): void {}
 }
